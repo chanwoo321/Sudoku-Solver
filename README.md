a99fcab80264a895251eb85974a1b327631f2b62
chanwoo321 <jck3@illinois.edu>
2020-06-27 14:09:02 -0500

    Finished the framework for this puzzle. Can be run in a script

--- README.md
+++ README.md
@@ -2,5 +2,20 @@
 Hi! This is my first attempt at creating a personal project, so I hope you enjoy it. This code includes a solver.py file that has python code that solves
 any Sudoku puzzle by using a backtracking technique. For more information, I would check out https://en.wikipedia.org/wiki/Sudoku_solving_algorithms.
 
-This is still a work in progress, so check back soon!
+At the moment, the algorithm works on the command line or any other compiler that
+allows users to input lines.
 
+# How to use the solver
+When you run solver.py, you will be prompted to enter each line individually.
+Enter in each line by typing the 9 numbers in each row of the sudoku puzzle you
+would like to solve in order. If the square is blank, type in a 0. After this is
+done, the sudoku solver will print out the board that you typed in to show the board
+you have inputted followed by printing out the solution to that board. If no solution
+is found, the solver will not print out anything. Note that due to the brute-force
+algorithm used, if there are multiple solutions then all possible solutions will be
+printed out.
+
+# Possible future updates
+In the future, I may update this repository to have a GUI or make the script a bit
+cleaner in it's input and how boards are printed. Until then, feel free to enjoy
+what is available now and look at my code!
--- solver.py
+++ solver.py
@@ -3,21 +3,21 @@
 # This python file will provide executable python code that will solve sudoku
 # puzzles using a backtracking algorithm. See the readme for more information.
 
-# Accepts a board and a spot on the board (denoted by row and column) and checks
-# if putting that value on that spot of the board is valid
 
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
 ]
 
+# Accepts a board and a spot on the board (denoted by row and column) and checks
+# if putting that value on that spot of the board is valid
 def isValid(board, row, col, value):
     # Checks whether the inserted number would invalidate the column or row
     for x in range(0,9):
@@ -32,9 +32,48 @@ def isValid(board, row, col, value):
                 return False;
     return True;
 
+# Finds the next empty square for where values can be guessed
 def nextSquare(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if (board[row][col] == 0):
                 return row, col
-    return -1
+    return -1, -1
+
+# Prints out the board in a more readable fashion
+def printBoard(board):
+    for x in board:
+        print(x)
+    return
+
+# Actually solves the puzzle recursively
+def solve(board):
+    row, col = nextSquare(board)
+    if row == -1:
+        return True
+    for x in range(1,10):
+        if isValid(board, row, col, x):
+            board[row][col] = x;
+            if solve(board) == False:
+                board[row][col] = 0
+            else:
+                printBoard(board)
+                print()
+    return False
+
+
+# Code to run the sudoku puzzle by just running it
+print("Hi, welcome to the sudooku solver! Please enter your puzzle:")
+for x in range(9):
+    line = raw_input('Enter row #' + str(x+1) + ':   ')
+    info = int(line)
+    for y in range(8,-1,-1):
+        board[x][y] = info % 10
+        info /= 10
+print()
+print("Your board:")
+print()
+printBoard(board)
+print()
+print("Solution:")
+solve(board)
